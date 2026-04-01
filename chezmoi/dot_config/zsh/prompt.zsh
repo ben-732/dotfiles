97a12bc8888b7dc562cb37d241341d8f92207518
Ben McMurtrie <22252563+ben-732@users.noreply.github.com>
2026-04-02 09:28:46 +1300

    full chezmoi

--- chezmoi/dot_config/zsh/prompt.zsh
+++ chezmoi/dot_config/zsh/prompt.zsh
@@ -1,3 +1,5 @@
+echo "$HOME/.config/ohmyposh/me.omp.json"
+
 if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
-  eval "$(oh-my-posh init zsh --config $_ZSH_DIR/poshthemes/me.omp.json)";
+  eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/me.omp.json)";
 fi
--- work/.zsh/work.zsh
+++
@@ -1,3 +0,0 @@
-
-export t3="$HOME/work/tidy3"
-alias t3="cd $t3"
