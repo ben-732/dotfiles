d836f8373efcfaaf23b338ae30db2213036347e1
Ben McMurtrie <22252563+ben-732@users.noreply.github.com>
2026-04-06 11:27:16 +1200

    Chezmoi (#1)
    * progress
    
    * root chezmoi.toml
    
    * full chezmoi
    
    ---------
    
    Co-authored-by: Ben McMurtrie <Benjamin.McMurtrie@airnz.co.nz>

---
+++ chezmoi/dot_config/zsh/env.zsh
@@ -0,0 +1,10 @@
+# Node Version Manager setup
+export NVM_DIR="$HOME/.nvm"
+
+# pnpm
+export PNPM_HOME="$HOME/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
---
+++ chezmoi/dot_config/zsh/prompt.zsh
@@ -0,0 +1,5 @@
+echo "$HOME/.config/ohmyposh/me.omp.json"
+
+if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
+  eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/me.omp.json)";
+fi
