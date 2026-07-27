63826f69c227806deee71c0581e09c05d864b289
Ben McMurtrie <22252563+ben-732@users.noreply.github.com>
2026-07-27 17:48:01 -0600

    person env separated

--- chezmoi/dot_config/zsh/env.zsh
+++ chezmoi/dot_config/zsh/env.zsh
@@ -1,15 +1,2 @@
-export EDITOR=nvim
-
-# Node Version Manager setup
-export NVM_DIR="$HOME/.nvm"
-
-# pnpm
-export PNPM_HOME="$HOME/Library/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
-# pnpm end
-
 export ANTHROPIC_MODEL='sonnet'
 # export ANTHROPIC_DEFAULT_OPUS_MODEL='claude-opus-4-7[1m]'
